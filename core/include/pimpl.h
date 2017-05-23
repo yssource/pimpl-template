ea2132649bafcd5cdbc4b5c302ec266b6db00766
Andy Sittig <andy.sittig@hotmail.com>
2017-05-23 10:30:36 +0200

    Refactor Pimpl class implementation.

--- core/include/pimpl.h
+++ core/include/pimpl.h
@@ -1,87 +1,109 @@
 #ifndef PIMPL_H
 #define PIMPL_H
 
-template <class Handle, class Impl>
+template <class Class, class ClassPrivate>
 class Pimpl
 {
-private:
-    Impl *_make() const
-    {
-        return ((Handle const *)this)->make();
-    }
-
-    void _unmake(Impl *p) const
-    {
-        ((Handle const *)this)->unmake(p);
-    }
-
-    Impl *_clone(Impl *p) const
-    {
-        return ((Handle const *)this)->clone(p);
-    }
-
-    void swap(Pimpl &other)
-    {
-        Impl *temp = d_ptr;
-        d_ptr = other.d_ptr;
-        other.d_ptr = temp;
-    }
-
 public:
     explicit Pimpl();
-    ~Pimpl();
-
     Pimpl(const Pimpl &other);
     Pimpl &operator=(const Pimpl &other);
 
-    // Fallbacks
-    static Impl *make()
-    {
-        return new Impl;
-    }
+protected:
+    // Disable destruction through base class pointer
+    // (see http://www.gotw.ca/publications/mill18.htm)
+    ~Pimpl();
 
-    static void unmake(Impl *p)
-    {
-        delete p;
-    }
+    // Fallbacks if the derived class does not implement them
+    static ClassPrivate *make();
+    static ClassPrivate *clone(ClassPrivate *p);
+    static void unmake(ClassPrivate *p);
 
-    static Impl *clone(Impl *p)
-    {
-        return new Impl(*p);
-    }
+private:
+    // Used to create an exception safe assignment operator
+    // (see https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Copy-and-swap)
+    void swap(Pimpl &other);
+
+    ClassPrivate *_make() const;
+    ClassPrivate *_clone(ClassPrivate *p) const;
+    void _unmake(ClassPrivate *p) const; // Prevents MSVC warning C4150
 
 protected:
-    Impl *d_ptr;
+    ClassPrivate *d_ptr;
 };
 
-template<class Handle, class Impl>
-Pimpl<Handle, Impl>::Pimpl() :
-    d_ptr(_make())
+template<class Class, class ClassPrivate>
+Pimpl<Class, ClassPrivate>::Pimpl()
+    : d_ptr(_make())
 {
 
 }
 
-template<class Handle, class Impl>
-Pimpl<Handle, Impl>::~Pimpl()
-{
-    _unmake(d_ptr);
-    d_ptr = 0;
-}
-
-template<class Handle, class Impl>
-Pimpl<Handle, Impl>::Pimpl(const Pimpl &other) :
-    d_ptr(_clone(other.d_ptr))
+template<class Class, class ClassPrivate>
+Pimpl<Class, ClassPrivate>::Pimpl(const Pimpl &other)
+    : d_ptr(_clone(other.d_ptr))
 {
 
 }
 
-template<class Handle, class Impl>
-Pimpl<Handle, Impl> &Pimpl<Handle, Impl>::operator=(const Pimpl &other)
+template<class Class, class ClassPrivate>
+Pimpl<Class, ClassPrivate> &Pimpl<Class, ClassPrivate>::operator=(const Pimpl &other)
 {
     Pimpl copy(other);
     swap(copy);
 
     return *this;
+}
+
+template<class Class, class ClassPrivate>
+Pimpl<Class, ClassPrivate>::~Pimpl()
+{
+    _unmake(d_ptr);
+    d_ptr = 0;
+}
+
+template<class Class, class ClassPrivate>
+ClassPrivate *Pimpl<Class, ClassPrivate>::make()
+{
+    return new ClassPrivate;
+}
+
+template<class Class, class ClassPrivate>
+ClassPrivate *Pimpl<Class, ClassPrivate>::clone(ClassPrivate *p)
+{
+    return new ClassPrivate(*p);
+}
+
+template<class Class, class ClassPrivate>
+void Pimpl<Class, ClassPrivate>::unmake(ClassPrivate *p)
+{
+    delete p;
+}
+
+template<class Class, class ClassPrivate>
+void Pimpl<Class, ClassPrivate>::swap(Pimpl &other)
+{
+    ClassPrivate *tmp = d_ptr;
+    d_ptr = other.d_ptr;
+    other.d_ptr = tmp;
+}
+
+template<class Class, class ClassPrivate>
+ClassPrivate *Pimpl<Class, ClassPrivate>::_make() const
+{
+    return ((Class const*)this)->make();
+}
+
+template<class Class, class ClassPrivate>
+ClassPrivate *Pimpl<Class, ClassPrivate>::_clone(ClassPrivate *p) const
+{
+    return ((Class const*)this)->clone(p);
+}
+
+template<class Class, class ClassPrivate>
+void Pimpl<Class, ClassPrivate>::_unmake(ClassPrivate *p) const
+{
+    ((Class const*)this)->unmake(p);
 }
 
 #endif // PIMPL_H
